fa5f0061c1ad1c4d9bb54565bf8d441ca941e73d
samuelwoldegiorgis <samgmil19@gmail.com>
2020-05-05 11:49:24 -0400

    Developing Functions

---
+++ src/com/syntax/class04/Constants.java
@@ -0,0 +1,13 @@
+package com.syntax.class04;
+
+public class Constants {
+
+	public static final String CHROME_DRIVER_PATH=System.getProperty("user.dir")+"/drivers/chromedriver";
+
+	public static final String GECKO_DRIVER_PATH=System.getProperty("user.dir")+"/drivers/geckodriver";
+
+	public static final String CONFIGURATION_FILEPATH=System.getProperty("user.dir")+"/configs/configuration.properties";
+}
+
+
+
